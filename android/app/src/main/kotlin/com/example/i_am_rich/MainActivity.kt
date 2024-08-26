90be32cc61afa18dafb8e05cda391ea1d427a4ca
duongm8 <nguyentoduong,2019@gmail.com>
2024-08-26 20:30:28 +0700

    lab1

---
+++ android/app/src/main/kotlin/com/example/i_am_rich/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('assets/diamond.jpg'),
+          ),
+        ),
+        backgroundColor: Colors.blueGrey,
+      ),  
+    );
+  }
+}
